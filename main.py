952f178f79a850a883ba86014bb2642cee854bc5
Brien <brienmicro@gmail.com>
2026-07-03 18:44:46 -0400

    All items in the list are now active and work, Milestone 1 Complete

--- main.py
+++ main.py
@@ -19,19 +19,30 @@ def delete_task():
     print(show_list())
     task_to_delete = input('What item would you like to remove from the list, use a number')
     task_number = int(task_to_delete)
-    to_do_list.pop(task_number - 1)
+    task_string = to_do_list.pop(task_number - 1)
+    
 #function for adding items to completed list 
+def task_finished():
+    print(show_list())
+    task_to_move = input('What task have you finished')
+    task_number = int(task_to_move)
+    task_string = to_do_list.pop(task_number - 1)
+    completed_tasks.append(task_string)
 
 while True: 
     print(intro_msg)
-    ask = input('1. See list ' '2. Add task ' '3. Delete Task ' '4. Add to completed')
+    ask = input('1. See list ' '2. Add task ' '3. Delete Task ' '4. Add to completed ' '5. Quit' )
     if ask == '1' or ask == 'See list':
         print(show_list())
     elif ask == '2' or ask == 'Add task':
         add_task()
     elif ask == '3' or ask == 'Delete task':
         delete_task()
-    elif ask == 4 or ask == 'Add to completed':
-        pass
+    elif ask == '4' or ask == 'Add to completed':
+        task_finished()
+        print(completed_tasks)
+    elif ask == '5' or ask == 'quit':
+        break
+
 
 
